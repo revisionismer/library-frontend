1f5a34e9ae78569fe3e553ff0037a0ef55037a43
revisionismer <cherry2137@naver.com>
2025-02-09 21:02:53 +0900

    11. 도서 리스트 불러오기 api 연동

--- src/pages/board/BoardList.js
+++ src/pages/board/BoardList.js
@@ -1,9 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, json, useLocation, useNavigate, useParams } from 'react-router-dom';
+
+import axios from 'axios';
+
+import Base64 from 'base-64';
 
 import '../../assets/css/board/boardList.css';
 
 const BoardList = () => {
+
+    const navigate = useNavigate();
+
+    var ACCESS_TOKEN = getCookie('access_token');
+
+    function getCookie(key) {
+
+        let result = null;
+        let cookie = document.cookie.split(';');
+
+        cookie.some(function (item) {
+            item = item.replace(' ', '');
+
+            let dic = item.split('=');
+
+            if (key === dic[0]) {
+                result = dic[1];
+                return true;
+            }
+            return false;
+        });
+        return result;
+    }
+
+    // 2024-07-17 : base64로 로그인 정보 꺼내오기
+    // 2024-07-18 : 토큰이 없다면 서버에서 예외터지도록 변경
+    let payload;
+    let loginUser;
+
+    if (ACCESS_TOKEN != null) {
+        payload = ACCESS_TOKEN.substring(ACCESS_TOKEN.indexOf('.') + 1, ACCESS_TOKEN.lastIndexOf('.'));
+        loginUser = JSON.parse(Base64.decode(payload));
+    }
+
+    function deleteCookie(key) {
+        document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+
+    const [boards, setBoards] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
+
     return (
         <>
             <div className='container'>
