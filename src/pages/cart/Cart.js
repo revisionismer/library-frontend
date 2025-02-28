7be0a19901d90b7e9cd171689ea84bf1f7e801d0
revisionismer <cherry2137@naver.com>
2025-02-28 11:40:49 +0900

    16. 장바구니 페이지에서 상품 리스트 주문하는 api 연동

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -9,6 +9,8 @@ import none from '../../assets/img/none.gif';
 
 import '../../assets/css/cart/cart.css';
 
+import qs from 'qs';
+
 const Cart = () => {
 
     const navigate = useNavigate();
@@ -93,6 +95,65 @@ const Cart = () => {
         getCarts();
     }, [])
 
+    useEffect(() => {
+        document.querySelector('#cartOrderBtn').addEventListener('click', (e) => {
+            var checkboxes = document.querySelectorAll('input[name="selectBook"]:checked');
+
+            var obj = new Object();
+
+            checkboxes.forEach((checkbox, index) => {
+                var tr = checkbox.parentNode.parentNode;
+                var td = tr.childNodes;
+                // obj.push(checkbox.id.slice(11));
+
+                obj[index] = {
+                    cartId: checkbox.id.slice(11),
+                    cartItemId: td[1].textContent,
+                    itemNm: td[2].textContent,
+                    price: td[3].textContent,
+                    count: td[4].childNodes[0].value
+                }
+
+            })
+
+            console.log(obj);
+
+            axios.post(`http://127.0.0.1:8080/api/orders/s/cartItem`,
+                JSON.stringify(obj),
+                {
+                    headers: {
+                        'Authorization': 'Bearer ' + ACCESS_TOKEN,
+                        'Content-Type': 'application/json; charset=UTF-8'
+
+                    }
+                }
+            ).then(function (res) {
+                console.log(res);
+
+
+            }).catch(function (res) {
+                console.log(res);
+
+                if (res.code === "ERR_NETWORK") {
+                    console.log("서버와의 연결이 되어 있지 않습니다.");
+                    return false;
+
+                }
+
+                if (res.response.status === 500 || res.response.status === 400 || res.response.status === 401 || res.response.status === 403) {
+                    // 2024-03-28 : alert가 두번씩 호출됨 고민해봐야함 : index.js에서 문제됨
+                    alert(res.response.data.message);
+
+                    // 2024-04-12 : 무슨 이유인지 GET 방식에서는 403일때 서버에서 쿠키 삭제가 안되어 클라이언트 단에서 직접 삭제
+                    deleteCookie('access_token');
+                    navigate("/signin");
+                    return;
+                }
+            })
+
+        })
+    }, [])
+
     return (
         <>
             <div className='container'>
@@ -108,11 +169,9 @@ const Cart = () => {
                             <div id='cart_area_btn'>
                                 <div className="row align-items-md-stretch">
                                     <div className="col-md-12">
-                                        <form name="clearForm" method="post">
-                                            <input type="hidden" name="_method" value="delete" />
-                                            <Link className="btn btn-danger" style={{ float: 'left' }}>삭제하기</Link>
-                                            <button to='/BookMarket/order/1' className="btn btn-success text-right" style={{ float: 'right' }}>주문하기</button>
-                                        </form>
+                                        <input type="hidden" name="_method" value="delete" />
+                                        <button id='cartCancelBtn' className="btn btn-danger" style={{ float: 'left' }}>삭제하기</button>
+                                        <button id='cartOrderBtn' className="btn btn-success text-right" style={{ float: 'right' }}>주문하기</button>
                                     </div>
                                 </div>
                             </div>
@@ -132,30 +191,42 @@ const Cart = () => {
                                         <thead>
                                             <tr>
                                                 <th></th>
+                                                <th>cartItemId</th>
                                                 <th>도서</th>
                                                 <th>가격</th>
                                                 <th>수량</th>
-                                                <th>소계</th>
                                                 <th>비고</th>
                                                 <th>-</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>
-                                                    <input type='checkbox' id='selectBook' name='selectBook' />
-                                                </td>
-                                                <td>나루토 1권</td>
-                                                <td>12000원</td>
-                                                <td>3</td>
-                                                <td>3</td>
-                                                <td>신청 중</td>
-                                                <td>
-                                                    <Link>
-                                                        <span className="badge text-bg-danger">재고 부족</span>
-                                                    </Link>
-                                                </td>
-                                            </tr>
+                                            {carts.map((cart, index) => {
+                                                return (
+                                                    <tr key={index}>
+                                                        <td>
+                                                            <input type='checkbox' id={`selectBook_${cart.cartId}`} name='selectBook' />
+                                                        </td>
+                                                        <td>{cart.cartItemId}</td>
+                                                        <td>{cart.itemNm}</td>
+                                                        <td>{cart.price}</td>
+                                                        <td><input type='number' id='count' name='count' defaultValue={cart.count} style={{ textAlign: 'center', width: '40px' }} /></td>
+                                                        <td>신청 중</td>
+                                                        <td>
+                                                            {cart.unitsInStock === 0 ?
+                                                                <Link>
+                                                                    <span className="badge text-bg-danger">재고 부족</span>
+                                                                </Link>
+                                                                :
+                                                                <Link>
+                                                                    <span className="badge text-bg-primary">주문 가능</span>
+                                                                </Link>
+                                                            }
+                                                        </td>
+                                                    </tr>
+
+                                                )
+                                            })}
+
                                         </tbody>
                                     </table>
                                 </form>
